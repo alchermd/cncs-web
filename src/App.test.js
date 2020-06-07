0a7419c65057ad30bba4985e4b7a1c88e7ece028
John Alcher M. Doloiras <alcher@directworksmedia.com>
2020-06-07 23:37:11 +0800

    Cleanup files and setup Travis CI with smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders 'Hello, World!'", () => {
+  act(() => {
+    render(<App />, container);
+  });  
+  expect(container.textContent).toBe("Hello, World!");
 });
